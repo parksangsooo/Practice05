0f9587c3c87114aaed8d6f6b20f191645f95a0ba
박상수 <qkzn326@gmail.com>
2024-03-29 18:12:24 +0900

    240329 커밋

--- src/com/javaex/ex08/Account.java
+++ src/com/javaex/ex08/Account.java
@@ -6,7 +6,36 @@ public class Account {
     private int balance;
     
     //생성자 작성
-    
+        
+    public void Account (String accountNo) {
+    	this.accountNo = accountNo;
+    	int balace = 0;
+        }
     //필요한 메소드 작성
+    public String getAccountNo() { 	// 계좌번호 조회
+    	return this.accountNo;
+    }
+    
+    public int getBalance() {  // 잔액조회
+    	return this.balance;
+    }
+    
+    
+    public void setBalance(int balance) {  // 잔액조회
+    	this.balance = balance;
+    }
+    
+    public void deposit() { // 입금
+    	return balance += 
+    }
+    
+    public void withdraw() {	// 출금
+    	
+    }
+    
+    public void showBalance() {		
+    	
+    }
+   
 
 }
